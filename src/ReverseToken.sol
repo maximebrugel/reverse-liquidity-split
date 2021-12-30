01e08712e4f031f5307503b7fcf6620851fd7c1a
axxe.eth <maxime.brugel@gmail.com>
2021-12-30 23:20:52 +0100

    init token

---
+++ src/ReverseToken.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+
+import "solmate/tokens/ERC20.sol";
+
+contract ReverseToken is ERC20("Reverse Token", "REV", 18) {}
--- src/test/ReverseToken.t.sol
+++ src/test/ReverseToken.t.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity 0.8.10;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
 
 import "ds-test/test.sol";
 
